7bdde066f3e82c7f533b5b4ca0992b2717ad215c
Mohamed Abdelrahman <mohamedbdelrahman2@gmail.com>
2024-05-12 02:35:46 +0300

    create uploadImage command and handler add reviewer endpoints

--- src/Core/InsightHive.Application/Interfaces/Persistence/IReviewerRepository.cs
+++ src/Core/InsightHive.Application/Interfaces/Persistence/IReviewerRepository.cs
@@ -10,10 +10,10 @@ namespace InsightHive.Application.Interfaces.Persistence
 {
     public interface IReviewerRepository : IRepository<Reviewer>
     {
+        Task<Reviewer> GetByIdWithReviewsAndBadgesAsync(int id);
         Task<Reviewer> GetByIdWithUserAsync(int id);
         Task<Reviewer> GetByUserIdWithUserAsync(int id);
         Task AddImagePathAsync(int id, string path);
-
         Task<List<Reviewer>> CalculateTopContributorsAsync();
     }
 }
--- src/Core/InsightHive.Application/UseCases/Reviewers/Query/GetReviewer/GetReviewerQuery.cs
+++ src/Core/InsightHive.Application/UseCases/Reviewers/Query/GetReviewer/GetReviewerQuery.cs
@@ -1,4 +1,5 @@
-﻿using MediatR;
+﻿using InsightHive.Application.Responses;
+using MediatR;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace InsightHive.Application.UseCases.Reviewers.Query.GetReviewer
 {
-    public class GetReviewerQuery : IRequest<ReviewerDetailsDto>
+    public class GetReviewerQuery : IRequest<BaseResponse<ReviewerDetailsDto>>
     {
         public int Id { get; set; }
     }
