25639c66009595eaf9e264db7d390e26c63ee2dd
Mohamed Abdelrahman <mohamedbdelrahman2@gmail.com>
2024-05-14 18:14:16 +0300

    edit owner model

--- src/Core/InsightHive.Domain/Entities/Owner.cs
+++ src/Core/InsightHive.Domain/Entities/Owner.cs
@@ -6,8 +6,6 @@
         public int UserId { get; set; }
         public User User { get; set; }
         public int BusinessId { get; set; }
-        public int UserId { get; set; }
-        public User User { get; set; }
         public Business Business { get; set; }
     }
 }
