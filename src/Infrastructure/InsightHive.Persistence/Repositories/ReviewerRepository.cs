bc5cfb9e598c43be41da83d14c56efe88d8d63e9
Mohamed Abdelrahman <mohamedbdelrahman2@gmail.com>
2024-05-17 00:50:35 +0300

    implement reviewer interface

---
+++ src/Infrastructure/InsightHive.Persistence/Repositories/ReviewerRepository.cs
@@ -0,0 +1,45 @@
+﻿using InsightHive.Application.Interfaces.Persistence;
+using InsightHive.Domain.Entities;
+using InsightHive.Persistence.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsightHive.Persistence.Repositories
+{
+    public class ReviewerRepository : BaseRepository<Reviewer>, IReviewerRepository
+    {
+        public ReviewerRepository(InsightHiveDbContext context) : base(context){ }
+
+        public async Task AddImagePathAsync(int id, string path)
+        {
+            var reviewer = await GetByIdAsync(id);
+            reviewer.Image = path;
+            await _context.SaveChangesAsync();
+        }
+
+        public Task<List<Reviewer>> CalculateTopContributorsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Reviewer> GetByIdWithReviewsAndBadgesAsync(int id)
+        {
+            return await _context.Reviewers.Include(e => e.Reviews)
+                .Include(e => e.Badges).FirstAsync(e => e.Id == id);
+        }
+
+        public async Task<Reviewer> GetByIdWithUserAsync(int id)
+        {
+            return await _context.Reviewers.Include(e => e.User).FirstAsync(e => e.Id == id);
+        }
+
+        public async Task<Reviewer> GetByUserIdWithUserAsync(int id)
+        {
+            return await _context.Reviewers.Include(e => e.User).FirstAsync(e => e.UserId == id);
+        }
+    }
+}
