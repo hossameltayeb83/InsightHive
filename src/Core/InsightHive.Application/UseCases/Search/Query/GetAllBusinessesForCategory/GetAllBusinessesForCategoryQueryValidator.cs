43eabab04956ac61be53640e42bdbebda981acbc
Hossam Eltayeb <hossameltayeb83@gmail.com>
2024-05-19 03:17:10 +0300

    changing propery name from query to search

--- src/Core/InsightHive.Application/UseCases/Search/Query/GetAllBusinessesForCategory/GetAllBusinessesForCategoryQueryValidator.cs
+++ src/Core/InsightHive.Application/UseCases/Search/Query/GetAllBusinessesForCategory/GetAllBusinessesForCategoryQueryValidator.cs
@@ -6,7 +6,7 @@ namespace InsightHive.Application.UseCases.Search.Query.GetAllBusinessesForCateg
     {
         public GetAllBusinessesForCategoryQueryValidator()
         {
-            RuleFor(e => e.Query)
+            RuleFor(e => e.Search)
                 .NotEmpty().WithMessage("{PropertyName} is required.")
                 .MaximumLength(50).WithMessage("{PropertyName} must not exceed 50 characters.")
                 .MinimumLength(3).WithMessage("{PropertyName} must not be less than 3 characters.");
